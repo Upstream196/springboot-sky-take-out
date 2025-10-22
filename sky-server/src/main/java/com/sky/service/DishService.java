605acf285020b294abff2473de2d3430c0ac99d4
Upstream196 <2718301902@qq.com>
2025-10-22 14:47:11 +0800

    菜品修改功能实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,4 +19,10 @@ public interface DishService {
     void deleteBatch(List<Long> ids);
 
     void startOrStop(Integer status, Long id);
+
+    //根据id查询菜品和对应的口味数据
+    DishVO getByIdWithFlavor(Long id);
+
+    //根据id修改菜品基本信息和对应的口味信息
+    void updateWithFlavor(DishDTO dishDTO);
 }
