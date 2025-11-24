8d3dc473e09b9cf2136776a0a60ad9002b0689f1
Upstream196 <2718301902@qq.com>
2025-11-24 09:27:40 +0800

    用户下单代码功能开发

--- sky-pojo/src/main/java/com/sky/entity/Orders.java
+++ sky-pojo/src/main/java/com/sky/entity/Orders.java
@@ -2,8 +2,29 @@ package com.sky.entity;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Data
 public class Orders {
+    /**
+     * 订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消
+     */
+    public static final Integer PENDING_PAYMENT = 1;
+    public static final Integer TO_BE_CONFIRMED = 2;
+    public static final Integer CONFIRMED = 3;
+    public static final Integer DELIVERY_IN_PROGRESS = 4;
+    public static final Integer COMPLETED = 5;
+    public static final Integer CANCELLED = 6;
+
+    /**
+     * 支付状态 0未支付 1已支付 2退款
+     */
+    public static final Integer UN_PAID = 0;
+    public static final Integer PAID = 1;
+    public static final Integer REFUND = 2;
+
+
     private Long id;
     //下单用户id
     private Long userId;
@@ -21,6 +42,26 @@ public class Orders {
     private String number;
     //订单状态
     private Integer status;
-
-
+    //实收金额
+    private BigDecimal amount;
+    //支付状态 0未支付 1已支付 2退款
+    private Integer payStatus;
+    //下单时间
+    private LocalDateTime orderTime;
+    //结账时间
+    private LocalDateTime checkoutTime;
+    //支付方式 1微信 2支付宝
+    private Integer payMethod;
+    //备注
+    private String remark;
+    //预计送达时间
+    private LocalDateTime estimatedDeliveryTime;
+    //配送状态 1立即送出 0选择具体时间
+    private Integer deliveryStatus;
+    //打包费
+    private int packAmount;
+    //餐具数量
+    private int tablewareNumber;
+    //餐具数量状态 1按量提供 0选择具体数量
+    private Integer tablewareStatus;
 }
