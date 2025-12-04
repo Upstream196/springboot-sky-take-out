9c278746518a0918bed5f5f0e105c6a6444eb82c
Upstream196 <2718301902@qq.com>
2025-12-04 16:50:58 +0800

    订单搜索功能模块开发

---
+++ sky-pojo/src/main/java/com/sky/dto/OrdersConfirmDTO.java
@@ -0,0 +1,12 @@
+package com.sky.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class OrdersConfirmDTO implements Serializable {
+
+    private Long id;
+    //订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消 7退款
+    private Integer status;
+}
